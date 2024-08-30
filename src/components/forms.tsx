7ae9d51503054a9f1f4bed951e4cc955b740ff27
AndresBorda24 <anjart24@gmail.com>
2024-08-30 09:49:39 -0500

    XInputs

--- src/components/forms.tsx
+++ src/components/forms.tsx
@@ -1,3 +1,6 @@
+import React, { useEffect, useRef, useState } from "react"
+
+interface withChildren { children?: React.ReactNode }
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> { }
 export const AppInput: React.FC<InputProps> = ({ type = 'text', ...props }) => {
   return (
@@ -33,6 +36,66 @@ export const AppSelect: React.FC<React.SelectHTMLAttributes<HTMLSelectElement>> 
     <select
       {...props}
       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring focus:ring-aso-primary/20 focus:border-aso-primary block w-full px-2.5 py-1.5"
+    />
+  )
+}
+
+
+interface XContainerProps {
+  mainChildren: React.ReactNode,
+  children: React.ReactNode
+}
+export const XContainer: React.FC<XContainerProps> = ({
+  mainChildren,
+  children
+}) => {
+  const [showInput, setShowInput] = useState(false)
+  const container = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (showInput && container.current && !(container.current).contains(e.target as Node)) {
+        setShowInput(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [showInput])
+
+  useEffect(() => {
+    if (showInput)
+      (container.current?.querySelector(':first-child') as HTMLInputElement)?.focus()
+  }, [showInput])
+
+  return (
+    <div
+      ref={container}
+      onClick={() => setShowInput(true)}
+      title={!showInput ? "Da click para modificar." : undefined}
+      className={`p-1 rounded border border-transparent ${showInput ? '' : 'hover:border-blue-300'}`}
+    > { showInput ? mainChildren : children } </div>
+  )
+}
+
+interface XInputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export const XInput: React.FC<XInputProps&withChildren> = ({children,...props}) => {
+  return (
+    <XContainer
+      mainChildren={<AppInput {...props} />}
+      children={children}
+    />
+  )
+}
+
+interface XTextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+export const XTextarea: React.FC<XTextareaProps&withChildren> = ({children, ...props}) => {
+  return (
+    <XContainer
+      mainChildren={<AppTextarea {...props} />}
+      children={children}
     />
   )
 }
--- src/routes/Project.tsx
+++ src/routes/Project.tsx
@@ -1,6 +1,7 @@
 import { ActionFunctionArgs, useLoaderData } from "react-router-dom"
 import View from "../components/view"
 import { appFetch } from "../AppFetch"
+import { XInput, XTextarea } from "../components/forms"
 import { Project as ProjectType } from "../types"
 import { ProjectNotFound } from "../components/ProjectNotFound"
 
@@ -21,11 +22,19 @@ const ProjectView: React.FC = () => {
   const { data, error } = useLoaderData() as LoaderDataType
   if (data === null) return <ProjectNotFound />
 
-  const { title } = data
+  const { title, description } = data
 
   return (
     <View title={`Visualizando: ${title}`}>
+      <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Título:</span>
+      <XInput type="text" name="title" defaultValue={title}>
+        Esto solo aparecerá cuando no se muestre el input de modificación...
+      </XInput>
 
+      <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Descripción:</span>
+      <XTextarea name="desciption" defaultValue={description} rows={10}>
+        <p className="text-neutral-700">{description}</p>
+      </XTextarea>
     </View>
   )
 }
