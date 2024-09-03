3d25646a50b9e2ab0c5852df6dde6eac5f6a2ca6
AndresBorda24 <anjart24@gmail.com>
2024-09-03 16:51:20 -0500

    Actualizacon de due date

--- src/components/HelperDates.tsx
+++ src/components/HelperDates.tsx
@@ -1,24 +1,29 @@
+import { useId } from "react";
 import { AppInput, AppSelect, AppLabel } from "./forms"
-import { BaseButton } from "./button"
-
-// type hdType = 'day'|'week'|'month'|'year'
+import { BaseButton } from "./button";
 
 /**
  * La idea de este componente es poder determinar una fecha a partir de dias,
  * semanas, mesas o años.
 */
 interface Props {
+  defaultDate: string|null;
   className?: string;
   setDate: (d: string) => void
 }
-export const HelperDates: React.FC<Props> =  ({ setDate, className = ''}) => {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const data = new FormData(e.currentTarget)
-    const amountToAdd = parseInt(data.get('hdNumber') as string)
+export const HelperDates: React.FC<Props> =  ({ setDate, defaultDate = null, className = ''}) => {
+  const selectId = useId()
+  const amountId = useId()
 
-    let now = new Date()
-    switch (data.get('hdTypeSelect')) {
+  const handleSubmit = () => {
+    const option = (document.getElementById(selectId) as HTMLSelectElement)!.value
+    const amountToAdd = parseInt((document.getElementById(amountId) as HTMLInputElement)!.value)
+
+    let now = (defaultDate === null) ? new Date() : new Date(defaultDate)
+    if (now.toString() === 'Invalid Date') {
+      now = new Date()
+    }
+    switch (option) {
       case "day":
         now.setDate(now.getDate() + amountToAdd)
         break
@@ -30,7 +35,6 @@ export const HelperDates: React.FC<Props> =  ({ setDate, className = ''}) => {
         break
       default:
         now.setFullYear(now.getFullYear() + amountToAdd)
-        break
     }
 
     const date = now.toISOString().substring(0, 10)
@@ -39,26 +43,24 @@ export const HelperDates: React.FC<Props> =  ({ setDate, className = ''}) => {
 
   return (
     <div className={className}>
-      <form onSubmit={handleSubmit}>
-        <div className="flex items-center gap-2 mb-2">
-          <AppLabel>
-            Medida:
-            <AppSelect name="hdTypeSelect" defaultValue="day" className="min-w-24">
-              <option value="day">Días</option>
-              <option value="week">Semanas</option>
-              <option value="month">Meses</option>
-              <option value="year">Años</option>
-            </AppSelect>
-          </AppLabel>
+      <div className="flex items-center gap-2 mb-2">
+        <AppLabel>
+          Medida:
+          <AppSelect id={selectId} defaultValue="day" className="min-w-24">
+            <option value="day">Días</option>
+            <option value="week">Semanas</option>
+            <option value="month">Meses</option>
+            <option value="year">Años</option>
+          </AppSelect>
+        </AppLabel>
 
-          <AppLabel>
-            Cantidad:
-            <AppInput type="number" name="hdNumber" required min={1} className="w-16" defaultValue={1}/>
-          </AppLabel>
-        </div>
+        <AppLabel>
+          Cantidad:
+          <AppInput type="number" id={amountId} required min={1} className="w-16 !px-1" defaultValue={1} />
+        </AppLabel>
 
-        <BaseButton size="small" color="tertiary" type="submit" className="ml-auto block">Establecer</BaseButton>
-      </form>
+        <BaseButton size="small" color="tertiary" onClick={handleSubmit}>✔</BaseButton>
+      </div>
     </div>
   )
 }
--- src/routes/Project.tsx
+++ src/routes/Project.tsx
@@ -7,6 +7,7 @@ import { Project as ProjectType } from "../types"
 import { SelectPriority } from "../components/Priority"
 import { HelperDates } from "../components/HelperDates"
 import { XInput, XTextarea, AppInput } from "../components/forms"
+import { useEffect } from "react"
 
 export async function loader({ params }: ActionFunctionArgs) {
   const {data, error} = await appFetch<ProjectType>('GET', {
@@ -22,75 +23,93 @@ export async function loader({ params }: ActionFunctionArgs) {
   return null
 }
 
+async function updateProjectRequest({ id, body }: {id: number, body: any}) {
+  await appFetch<ProjectType>('PATCH', { url: `/projects/${id}/patch`, body })
+}
+
 const ProjectView: React.FC = () => {
-  const { title, description, started_at, priority, created_at, due_date, patchProject } = useProjectStore((state) => ({
-    title: state.title,
-    description: state.description,
-    started_at: state.started_at,
-    priority: state.priority,
-    created_at: state.created_at,
-    due_date: state.due_date,
-    patchProject: state.patchProject
-  }))
+  const {
+    id,
+    title,
+    description,
+    started_at,
+    priority,
+    created_at,
+    due_date,
+    patchProject
+  } = useProjectStore((state) => state)
+
+  useEffect(() => {
+    updateProjectRequest({id, body: { priority, due_date }})
+    const selectDueDate = document.querySelector('#project-form [name="due_date"]')
+
+    if (due_date && selectDueDate) {
+      (selectDueDate as HTMLInputElement).value = due_date
+    }
+  }, [priority, due_date])
+
 
   return (
     <View title={`Visualizando: ${title}`}>
       <section className="max-w-lg">
-        <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Fecha de Creación:</span>
-        <span className="block p-1 capitalize">{ formatDate(created_at) }</span>
+        <form onSubmit={(e) => e.preventDefault()} id="project-form">
+          <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Fecha de Creación:</span>
+          <span className="block p-1 capitalize">{ formatDate(created_at) }</span>
 
-        <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Título:</span>
-        <XInput
-          type="text"
-          name="title"
-          onChange={e => patchProject('title', e.target.value)}
-          onBlur={() => console.log('Esto es lo que ocurre en el onBlur')}
-          defaultValue={title}
-        > {title} </XInput>
+          <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Título:</span>
+          <XInput
+            type="text"
+            name="title"
+            onChange={e => patchProject('title', e.target.value)}
+            onBlur={() => updateProjectRequest({id, body: { title }})}
+            defaultValue={title}
+          > {title} </XInput>
 
-        <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Descripción:</span>
-        <XTextarea
-          name="desciption"
-          defaultValue={description}
-          rows={10}
-          onChange={e => patchProject('description', e.target.value)}
-          onBlur={() => console.log('Esto es lo que ocurre en el onBlur')}
-        > <p className="text-neutral-700 whitespace-break-spaces">{description}</p> </XTextarea>
+          <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Descripción:</span>
+          <XTextarea
+            name="desciption"
+            defaultValue={description}
+            rows={10}
+            onChange={e => patchProject('description', e.target.value)}
+            onBlur={() => updateProjectRequest({id, body: { description }})}
+          > <p className="text-neutral-700 whitespace-break-spaces">{description}</p> </XTextarea>
 
-        <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Prioridad</span>
-        <SelectPriority priority={priority} setPriority={(p) => patchProject('priority', p)} className="mb-3"/>
+          <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Prioridad</span>
+          <SelectPriority priority={priority} setPriority={(p) => patchProject('priority', p)} className="mb-3"/>
 
-        <div className="grid sm:grid-cols-2 gap-2 items-end">
-          <div>
-            <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">
-              Fecha de Inicio
-              <span className="text-neutral-400 text-[10px] font-normal pl-1">(Una vez establecida no se podrá cambiar)</span>
-            </span>
-            {
-              started_at
-                ? <span className="block p-1 capitalize">{ formatDate(started_at) }</span>
-                : ( <AppInput type="date" className="ml-1"/>)
-            }
-          </div>
-          <div>
-            <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Fecha de finalización estimada:</span>
-            <div className="flex gap-1 items-center">
-              <AppInput type="date" className="ml-1" defaultValue={due_date}/>
-              <SelectDateHelper setDate={(d => patchProject('due_date', d))} />
+          <div className="grid sm:grid-cols-2 gap-2 items-end">
+            <div>
+              <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">
+                Fecha de Inicio
+                <span className="text-neutral-400 text-[10px] font-normal pl-1">(Una vez establecida no se podrá cambiar)</span>
+              </span>
+              {
+                started_at
+                  ? <span className="block p-1 capitalize">{ formatDate(started_at) }</span>
+                  : ( <AppInput type="date" name="started_at" className="ml-1"/>)
+              }
+            </div>
+            <div>
+              <span className="text-neutral-400 font-bold text-[10px] inline-block pl-1">Fecha de finalización estimada:</span>
+              <div className="flex gap-1 items-center">
+                <AppInput type="date" className="ml-1" defaultValue={due_date} name="due_date" />
+                <SelectDateHelper date={due_date || null} setDate={(d => patchProject('due_date', d))} />
+              </div>
             </div>
           </div>
-        </div>
+        </form>
       </section>
     </View>
   )
 }
 
-interface SelectDateHelperProps { setDate: (d: string) => void }
-const SelectDateHelper: React.FC<SelectDateHelperProps> = ({ setDate }) => {
+interface SelectDateHelperProps { date: string|null; setDate: (d: string) => void }
+const SelectDateHelper: React.FC<SelectDateHelperProps> = ({ date = null, setDate }) => {
   return (
     <details className="relative">
       <summary className="list-none h-4 rounded-full bg-neutral-800 text-neutral-300 inline-grid leading-none place-content-center aspect-square cursor-pointer select-none">&bull;</summary>
       <HelperDates
+        defaultDate={date}
         setDate={setDate}
         className="absolute bg-neutral-50 p-2 rounded shadow-lg bottom-full right-0 border border-neutral-200 outline outline-offset-1 outline-1 outline-neutral-300"
       />
