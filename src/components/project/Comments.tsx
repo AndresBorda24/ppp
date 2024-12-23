c777d298a074dca752c65d116ad6c4b2246e50c0
AndresBorda24 <anjart24@gmail.com>
2024-12-23 11:24:44 -0500

    Filtrar Comentarios

--- src/components/project/Comments.tsx
+++ src/components/project/Comments.tsx
@@ -1,25 +1,59 @@
+import { useEffect, useState } from "react";
 import { useProjectStore } from "../../stores/Project";
 import { CommentItem } from "../comments/CommentItem";
+import { useDebounce } from "use-debounce";
+import { CommentWithTitle } from "../../types";
+import { AppInput } from "../forms";
 
 export const Comments: React.FC = () => {
   const { comments } = useProjectStore();
+  const [search, setSearch] = useState("");
+  const [debouncedSearch] = useDebounce(search, 300);
+  const [filteredComments, setFilteredComments] = useState<CommentWithTitle[]>(
+    []
+  );
+
+  useEffect(() => {
+    const trimmedSearch = debouncedSearch.trim();
+    if (!trimmedSearch) {
+      setFilteredComments(
+        comments.filter((comment) => comment.obs_type === "project")
+      );
+      return;
+    }
+
+    const searchResults = comments.filter((comment) => {
+      return (
+        comment.body.toLowerCase().includes(trimmedSearch.toLowerCase())
+      );
+    });
+
+    setFilteredComments(searchResults);
+  }, [debouncedSearch, comments]);
 
   return (
     <div className="w-full relative">
-      <header className="mb-2">
-        <h5 className="text-aso-secondary font-bold text-base mb-1">Comentarios</h5>
+      <header className="mb-4">
+        <h5 className="text-aso-secondary font-bold text-base mb-1">
+          Comentarios
+        </h5>
+
+        <AppInput
+          type="search"
+          defaultValue={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Buscar comentarios"
+        />
       </header>
-      {
-        (comments.length === 0)
-          ? <span>Aún no hay comentarios para este Proyecto.</span>
-          : (
-            <div role="list" className="flex flex-col space-y-6 overflow-auto">
-              {
-                comments.map(comment => (<CommentItem key={comment.id} comment={comment} />))
-              }
-            </div>
-          )
-      }
+      {filteredComments.length === 0 ? (
+        <span>Aún no hay comentarios para este Proyecto.</span>
+      ) : (
+        <div role="list" className="flex flex-col space-y-6 overflow-auto">
+          {filteredComments.map((comment) => (
+            <CommentItem key={comment.id} comment={comment} />
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
