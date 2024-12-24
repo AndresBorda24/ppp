fd8153470d4c21b487fe13f89833bd1b5f33ebee
AndresBorda24 <anjart24@gmail.com>
2024-12-24 08:32:37 -0500

    Oculatar comentarios para nueva tarea

--- src/components/task/TaskModalMobileComments.tsx
+++ src/components/task/TaskModalMobileComments.tsx
@@ -2,11 +2,15 @@ import { TaskModalSection } from "./TaskModalSection";
 import { useModalComments } from "../../hooks/useModalComments";
 import { useBreakpoint } from "../../hooks/breakpoints";
 import { CommentItem } from "../comments/CommentItem";
+import { NewComment } from "../comments/NewComment";
+import { useTaskModalStore } from "../../stores/TaskModal";
 
 export const TaskModalMobileComments: React.FC = () => {
   const { modalComments } = useModalComments();
+  const { task } = useTaskModalStore()
   const isLg = useBreakpoint("lg");
   if (isLg) return null;
+  if (! task.id) return null;
 
   return (
     <TaskModalSection
@@ -17,6 +21,10 @@ export const TaskModalMobileComments: React.FC = () => {
         </span>
       }
     >
+      <div className="mb-4">
+        <NewComment type={task.detail_type} id={task.id} />
+      </div>
+
       {modalComments.length === 0 ? (
         <span className="text-sm italic text-neutral-500">Aún no hay comentarios.</span>
       ) : (
